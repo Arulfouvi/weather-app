de58a6825e099d89e4a4fe0b058421f6feb04f77
Arulfouvi <Arul#fouvi.com>
2023-12-12 16:28:37 +0530

    firstcmt

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <weather-app/>
+</template>
+
+<script>
+import weatherApp from './components/weather-app.vue'
+
+
+export default {
+  components: { weatherApp },
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
